7e30adf98a0bc426357bd9dab18c8ee43f0a0f83
Ch.k.Rishi <chkrishi4@gmail.com>
2025-12-14 16:58:08 +0530

    Base code pushed

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export const Home = () => {
+  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>clickme</Button>
+    </div>
+  )
+}
+
+export default Home
